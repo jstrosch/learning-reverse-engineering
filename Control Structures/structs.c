b36b44024ce2879670f6ee407ae822ab8ac910d0
Josh Stroschein <josh@m9development.com>
2020-07-07 17:04:55 -0500

    updates structures example to align with YouTube video

--- Control Structures/structs.c
+++ Control Structures/structs.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
 
-struct my_struct {
-	char a; 	// 1 bytes        Offset 0
-	// 			   1 byte padding
-	short b; 	// 2 bytes        Offset 2
-	int c; 		// 4 bytes        Offset 4
-	float d; 	// 4 bytes        Offset 8
+struct aligned_struct { 
+	int a;		// 4 bytes		Offset 0
+	short b;	// 2 bytes		Offset +4
+	char c;		// 1 byte		Offset +6
+	char d;		// 1 byte		Offset +7
+};				// Total: 8 bytes
+
+struct padded_struct {
+	char a; 	// 1 bytes      Offset 0
+				// 1 byte 		padding
+	short b; 	// 2 bytes      Offset 2
+	int c; 		// 4 bytes      Offset 4
+	int* d; 	// 4 bytes      Offset 8
 }; 				// Total: 12 bytes
 
-struct foo1 {
+struct padded2_struct {
 	char *p; 		// 4 bytes Offset 0
 	char c;			// 1 byte  Offset +4
 					// 3 bytes padding
 	long x;			// 4 bytes Offset +8
-}					// Total: 12 bytes
-
-void demo(struct my_struct *ms) {
-
-	ms->a = 'j';
-	ms->b = 1;
-	ms->c = 32;
-	ms->d = 99;
-
-}
+};					// Total: 12 bytes
 
 int main(void) {
+	int z = 99;
 
-	struct my_struct*ms = (struct my_struct*)malloc(sizeof(struct my_struct));
+	struct aligned_struct as = {0};
+	as.a = 42;
+	as.b = 0x1FF;
+	as.c = 'a';
+	as.d = 'b';
 
-	demo(ms);
+	struct aligned_struct*as2 = (struct aligned_struct*)malloc(sizeof(struct aligned_struct));
+	as2->a = 43;
+	as2->b = 3;
+	as2->c = 'c';
+	as2->d = 'd';
 
-	struct foo1*my = (struct foo1*)malloc(sizeof(struct foo1));
-
-	char a = 'a';
-
-	my->p = &a;
-	my->c = 'b';
-	my->x = 1;
-
+	struct padded_struct*ps = (struct padded_struct*)malloc(sizeof(struct padded_struct));
+	ps->a = 'e';
+	ps->b = 0xAA;
+	ps->c = 44;
+	ps->d = &z;
 }
